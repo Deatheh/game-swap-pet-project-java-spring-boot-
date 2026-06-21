2170dd1bf839c3a3f49c7899751152d870de5ef0
Daniil <kazancevdanila2015@yandex.ru>
2026-06-21 16:04:50 +0300

    fix: min password > 8 char

--- src/main/groovy/petproject/gameswap/dto/auth/RegisterRequest.java
+++ src/main/groovy/petproject/gameswap/dto/auth/RegisterRequest.java
@@ -20,6 +20,7 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 200, message = "Password must be between 8 and 200 characters")
     private String password;
 
 }
