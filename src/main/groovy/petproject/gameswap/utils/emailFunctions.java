0ac7e7f71b1dda8d44a88cc5907b439c752bb755
Daniil <kazancevdanila2015@yandex.ru>
2026-06-21 04:58:09 +0300

    remove: emailFunctions.java

--- src/main/groovy/petproject/gameswap/utils/emailFunctions.java
+++
@@ -1,9 +0,0 @@
-package petproject.gameswap.utils;
-
-public class EmailFunctions {
-
-    public static boolean isEmailValid(String email){
-        return email.contains("@") && email.contains(".")
-                && email.indexOf("@") < email.lastIndexOf(".");
-    }
-}
